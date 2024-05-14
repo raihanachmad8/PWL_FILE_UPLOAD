915333a66471e967bb1d38665d6a164a346001ed
raihanachmad8 <raihanachmad8@gmail.com>
2024-05-14 09:50:57 +0700

    jobsheet(12): C. VALIDASI FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -13,22 +13,11 @@ class FileUploadController extends Controller
     public function prosesFileUpload(Request $request) {
         // return "Pemrosesan file upload di sini";
         // dump($request->berkas);
+        $request->validate([
+            'berkas' => 'required|file|image|max:500'
+        ]);
 
-        if ($request->hasFile('berkas')) {
-            echo "path() : " . $request->berkas->path();
-            echo "<br>";
-            echo "extension() : " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension() : " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType() : " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName() : " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize() : " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada file yang diupload";
-        }
+        echo $request->berkas->getClientOriginalName(). " lolos validasi";
 
     }
 }
